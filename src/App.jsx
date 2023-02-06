5440a75c7675c5a0c213091222b646ce1dc90743
Vihang Supare <vihang7660@gmail.com>
2023-02-06 17:25:50 +0530

    linked pages to the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Checkout from './pages/Checkout';
+import OrderConfirmation from './pages/Order_confirmation';
+import ProductListing from './pages/Product_listing';
+
 function App() {
-  return <div>Namaste</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<ProductListing />} />
+        <Route path='/checkout' element={<Checkout />} />
+        <Route path='/order-confirmation' element={<OrderConfirmation />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
